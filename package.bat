abb659e336ba2070d6bd88f2c8047692aa652981
Martin Lorentzon <martin.lorentzon.work@gmail.com>
2025-02-13 17:53:31 +0100

    housekeeping

--- package.bat
+++ package.bat
@@ -8,7 +8,15 @@ for %%a in ("%CURR_DIR%") do (
   set "DIR_NAME=%%~na"
 )
 
-set "ZIP_NAME=%DIR_NAME%.zip"  REM Use the directory name for the zip file
+:: Prompt the user for a version number
+set /p "VERSION=Enter version number (e.g., 1_0, 2_1, or leave blank): "
+
+:: Add the version suffix to the directory name if provided
+if not "%VERSION%"=="" (
+  set "DIR_NAME=%DIR_NAME%_%VERSION%"
+)
+
+set "ZIP_NAME=%DIR_NAME%.zip"  REM Use the directory name (with version) for the zip file
 set "SEVENZIP_PATH=C:\Program Files\7-Zip\7z.exe" REM Replace with your 7-Zip path
 
 :: Create the zip archive with folder structure
